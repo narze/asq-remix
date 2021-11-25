3fcf5cccdd0084ee92e1202fc91d366d0cc37f90
Manassarn Manoonchai <manassarn@gmail.com>
2021-11-25 21:54:43 +0700

    Vercel deployment

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,6 @@ module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002
-};
+  serverBuildDirectory: "api/build",
+  devServerPort: 8002,
+}
